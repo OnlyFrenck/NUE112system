60186a5d17724f216566db5671bdd86af5e47926
onlylogistics <onlyfrenckyt@gmail.com>
2024-09-03 11:07:41 +0200

    Aggiunti nuovi font e modificati file script e style

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let t=0
+function check() {
+    var email = document.getElementById("email").value;
+    var psw = document.getElementById("password").value;
+    console.log(email+" "+psw);
+    if(email.endsWith("nue112-fvg.it"))
+        document.cookie = "foo=1; domain=onlyfrenck.github.io";
+    else
+        window.location.href = "https://kerlemn.github.io/repartometro"
+}
